feb85fac007a7c16545364adf8d9b2239d0041ff
Dan Sirbu <danu_91@mail.ru>
2021-03-24 21:54:25 +0200

    First Commit

---
+++ src/clientServerCW/ChatClient4.java
@@ -0,0 +1,130 @@
+package clientServerCW;
+
+import java.awt.event.ActionEvent;
+
+
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+
+public class ChatClient4 {
+
+    String serverAddress;
+    Scanner in;
+    PrintWriter out;
+    JFrame frame = new JFrame("GaduGadu");
+    JTextField textField = new JTextField(50);
+    JTextArea messageArea = new JTextArea(16, 50);
+
+
+    public ChatClient4(String serverAddress) {
+        this.serverAddress = serverAddress;
+
+
+// creating chatterbox:
+        textField.setEditable(false);
+        messageArea.setEditable(false);
+        frame.getContentPane().add(textField, BorderLayout.SOUTH);
+        frame.getContentPane().add(new JScrollPane(messageArea), BorderLayout.CENTER);
+        frame.pack();
+
+        textField.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                out.println(textField.getText());
+                textField.setText("");
+            }
+        });
+    }
+
+
+    private String getId() {
+        return JOptionPane.showInputDialog(
+                frame,                               // window
+                "What is your ID?",
+                "ID",                             // window title
+                JOptionPane.PLAIN_MESSAGE);     // type of window
+    }
+
+    private String getClientIP() {
+        return JOptionPane.showInputDialog(frame, "What is your IP number?");
+    }
+
+    private String getClientPort() {
+        return JOptionPane.showInputDialog(frame, "Which port would you like to use today?");
+    }
+
+    private String getServerIP() {
+        return JOptionPane.showInputDialog(frame, "What is the IP of the server that you wish to connect to?");
+    }
+
+    private String getServerPort() {
+        return JOptionPane.showInputDialog(frame, "What is the port number of the server?");
+    }
+
+
+    private void run() throws IOException {
+        try {
+            Socket socket = new Socket(serverAddress, 59008);
+
+            in = new Scanner(socket.getInputStream());
+            out = new PrintWriter(socket.getOutputStream(), true);
+
+
+            while (in.hasNextLine()) {
+
+                String line = in.nextLine();
+
+                if (line.startsWith("SUBMITID")) {
+                    out.println(getId());
+                } else if (line.startsWith("SUBMITIP1")) {
+                    out.println(getClientIP());
+                } else if (line.startsWith("SUBMITPORT1")) {
+                    out.println(getClientPort());
+                } else if (line.startsWith("SUBMITIP2")) {
+                    out.println(getServerIP());
+                } else if (line.startsWith("SUBMITPORT2")) {
+                    out.println(getServerPort());
+                } else if (line.startsWith("IDACCEPTED")) {
+                    this.frame.setTitle("ChatWindow - " + line.substring(11));  // windows title will be "ChatWindow - ID of the user"
+                    textField.setEditable(true);
+                }
+
+                // member should be informed about being 1-st at start-up (something wrong, needs to be revised:
+                else if (line.startsWith("COORDINATOR")) {
+                    this.frame.setTitle("ChatWindow - " + line.substring(11) + " - COORDINATOR");
+                } else if (line.startsWith("MESSAGE")) {
+                    messageArea.append(line.substring(8) + "\n");
+                }
+            }
+
+        } finally {
+            frame.setVisible(false);
+            frame.dispose();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        if (args.length != 1) {
+            System.err.println("Pass the server IP as the sole command line argument");
+            return;
+        }
+
+        ChatClient4 client = new ChatClient4(args[0]);
+        client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        client.frame.setVisible(true);
+        client.run();
+    }
+} 
---
+++ src/clientServerCW/ChatServer4.java
@@ -0,0 +1,200 @@
+package clientServerCW;
+
+import java.io.IOException;
+
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Set;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.*;
+import java.lang.System;
+
+
+public class ChatServer4 {
+
+    private static Set<String> ids = new HashSet<>();
+    private static Set<PrintWriter> writers = new HashSet<>();
+    private static List<String> ips1 = new ArrayList<>();
+    private static List<String> ips2 = new ArrayList<>();
+    private static List<String> ports1 = new ArrayList<>();
+    private static List<String> ports2 = new ArrayList<>();
+
+
+    // initialize and start our thread:
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("The chat server is running...");
+
+        ExecutorService pool = Executors.newFixedThreadPool(500);
+
+        try (ServerSocket listener = new ServerSocket(59008)) {
+
+            while (true) {
+                pool.execute(new Handler(listener.accept()));
+
+                Thread t = new Thread(new Handler(null));
+                t.start();
+            }
+        }
+    }
+
+
+    private static class Handler implements Runnable {
+        private String id;
+        private String ip1;
+        private String ip2;
+        private String port1;
+        private String port2;
+
+        private Socket socket;
+        private Scanner in;
+        private PrintWriter out;
+
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+
+        public void run() {
+
+            try {
+                in = new Scanner(socket.getInputStream());
+                out = new PrintWriter(socket.getOutputStream(), true);
+
+                //  Requesting an ID until we get a unique one:
+                while (true) {
+                    out.println("SUBMITID");
+                    id = in.nextLine();
+                    if (id == null) {
+                        return;
+                    }
+                    synchronized (ids) {
+                        if (!id.isEmpty() && !ids.contains(id)) {
+                            ids.add(id);
+                            break;
+                        }
+                    }
+                }
+
+
+                out.println("IDACCEPTED " + id);
+                for (PrintWriter writer : writers) {
+                    writer.println("MESSAGE " + id + " has joined");
+                }
+                writers.add(out);
+
+
+                // member should be informed about being 1-st at start-up (something wrong, needs to be revised:
+                while (true) {
+                    if (ids.size() == 1) {
+                        out.println("COORDINATOR" + id);
+                        break;
+                    }
+                    synchronized (ids) {
+                        if (!id.isEmpty()) {
+                            ids.add(id);
+                            break;
+                        }
+                    }
+                }
+
+
+                // I added separate while loop to every request:
+                while (true) {
+                    out.println("SUBMITIP1");   // client ip
+                    ip1 = in.nextLine();
+                    if (ip1 == null) {
+                        return;
+                    }
+                    synchronized (ips1) {
+                        if (!ip1.isEmpty()) {
+                            ips1.add(ip1);
+                            break;
+                        }
+                    }
+                }
+
+
+                //requesting clients port:
+                while (true) {
+                    out.println("SUBMITPORT1");
+                    port1 = in.nextLine();
+                    if (port1 == null) {
+                        return;
+                    }
+                    synchronized (ports1) {
+                        if (!port1.isEmpty()) {
+                            ports1.add(port1);
+                            break;
+                        }
+                    }
+                }
+
+                // asking for servers ip:
+                while (true) {
+                    out.println("SUBMITIP2");
+                    ip2 = in.nextLine();
+                    if (ip2 == null) {
+                        return;
+                    }
+                    synchronized (ips2) {
+                        if (!ip2.isEmpty()) {
+                            ips2.add(ip2);
+                            break;
+                        }
+                    }
+                }
+
+                // asking for servers port:
+                while (true) {
+                    out.println("SUBMITPORT2");
+                    port2 = in.nextLine();
+                    if (port2 == null) {
+                        return;
+                    }
+                    synchronized (ports2) {
+                        if (!port2.isEmpty()) {
+                            ports2.add(port2);
+                            break;
+                        }
+                    }
+                }
+
+                while (true) {
+                    String input = in.nextLine();
+                    if (input.toLowerCase().startsWith("/quit")) {
+                        return;
+                    }
+                    for (PrintWriter writer : writers) {
+                        writer.println("MESSAGE " + id + ": " + input);
+                    }
+                }
+
+
+            } catch (Exception e) {
+                System.out.println(e);
+
+            } finally {
+                if (out != null) {
+                    writers.remove(out);
+                }
+                if (id != null) {
+                    System.out.println(id + " is leaving");    // console output
+                    ids.remove(id);
+                    for (PrintWriter writer : writers) {
+                        writer.println("MESSAGE " + id + " has left");    // windows output 
+                    }
+                }
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+    }
+}
